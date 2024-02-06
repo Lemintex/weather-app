01e747b3f2fdb19c9e443c9b43628b3fb5bbf349
Lemintex <aaron_nobile@hotmail.co.uk>
2024-02-06 00:03:18 +0000

    Progress adding weather details

--- src/app/components/DayForecast.tsx
+++ src/app/components/DayForecast.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Container from './Container'
+import WeatherDetails from './WeatherDetails';
 
 export default function DayForecast() {
   return (
@@ -10,7 +11,8 @@ export default function DayForecast() {
       <p className='text-2xl'>Clouds</p>
     </Container>
     {/* Weather */}
-    <Container className='overflow-x-auto w-full flex-col bg-yellow-400'>
+    <Container className='flex overflow-x-auto w-screen flex-col bg-yellow-400'>
+      <WeatherDetails visibility={1} humidity={2} wind={{speed: 3, deg: 4}} sunrise={5} sunset={6} />
     </Container>
   </div>
   );
---
+++ src/app/components/WeatherDetails.tsx
@@ -0,0 +1,67 @@
+import React from 'react'
+
+export interface WeatherDetailProps {
+  visibility: number;
+  humidity: number;
+  wind: {
+    speed: number;
+    deg: number;
+  };
+  sunrise: number;
+  sunset: number;
+}
+
+export default function WeatherDetails(props: WeatherDetailProps) {
+  const {
+    visibility = 0,
+    humidity = 0,
+    wind = { speed: 0, deg: 0 },
+    sunrise = 0,
+    sunset = 0
+  } = props;
+  return (
+    <div className='flex justify-between'>
+      <SingleWeatherDetail
+        info='Visibility'
+        icon={<i className='fas fa-eye'></i>}
+        value={`${props.visibility} m`}
+      />
+
+      <SingleWeatherDetail
+        info='Humidity'
+        icon={<i className='fas fa-tint'></i>}
+        value={`${props.humidity}%`}
+      />
+
+      <SingleWeatherDetail
+        info='Wind'
+        icon={<i className='fas fa-wind'></i>}
+        value={`${props.wind.speed} m/s`}
+      />
+
+      <SingleWeatherDetail  
+        info='Sunrise'
+        icon={<i className='fas fa-sun'></i>}
+        value={new Date(props.sunrise * 1000).toLocaleTimeString()}
+      />
+
+      <SingleWeatherDetail
+        info='Sunset'
+        icon={<i className='fas fa-moon'></i>}
+        value={new Date(props.sunset * 1000).toLocaleTimeString()}
+      />
+    </div>
+  )
+}
+
+export interface SingleWeatherDetailProps {
+  info: string;
+  icon: React.ReactNode;
+  value: string;
+}
+
+function SingleWeatherDetail(props: SingleWeatherDetailProps) {
+  return (
+    <div className='flex flex-col justify-between px-5 items-center'>TEST</div>
+  )
+}
