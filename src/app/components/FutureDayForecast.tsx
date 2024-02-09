7cc36690fb2725f412338c080c0525312f43e1c9
Aaron Nobile <aaron.nobile@tribalgroup.com>
2024-02-09 16:37:55 +0000

    Progresss on future day panel

--- src/app/components/FutureDayForecast.tsx
+++ src/app/components/FutureDayForecast.tsx
@@ -2,13 +2,56 @@ import React from 'react'
 import { SingleWeatherDetailProps, WeatherDetailProps } from './WeatherDetails'
 import Container from './Container'
 
-interface FutureDayForecastProps extends SingleWeatherDetailProps {
+interface FutureDayForecastProps extends WeatherDetailProps {
 }
 
 export default function FutureDayForecast(props: FutureDayForecastProps) {
   return (
-    <Container className="bg-red-300 my-3">
+    <Container className="flex w-full bg-red-300 my-3">
+      <div className='w-fit mx-5'>
       test
+      </div>
+      {/*right bit*/}
+      <section className='flex overflow-x-auto w-screen justify-between'>
+      <SingleWeatherDetail
+          info='Visibility'
+          icon={<i className='fas fa-eye'></i>}
+          value={`m`}
+        />
+        <SingleWeatherDetail
+          info='Visibility'
+          icon={<i className='fas fa-eye'></i>}
+          value={`m`}
+        />
+        <SingleWeatherDetail
+          info='Visibility'
+          icon={<i className='fas fa-eye'></i>}
+          value={`m`}
+        />
+        <SingleWeatherDetail
+          info='Visibility'
+          icon={<i className='fas fa-eye'></i>}
+          value={`m`}
+        />
+        <SingleWeatherDetail
+          info='Visibility'
+          icon={<i className='fas fa-eye'></i>}
+          value={`m`}
+        />
+      </section>
     </Container>
+  )
+}
+
+function SingleWeatherDetail(props: SingleWeatherDetailProps) {
+  return (
+    <div className='flex flex-col justify-between px-5 items-center w-fit bg-gray-200'>
+      <p>
+        {props.info}
+      </p>
+      <p>
+        {props.value}
+      </p>
+    </div>
   )
 }
